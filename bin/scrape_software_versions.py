ccb5915f3e50d286c3cfae5b0b2d15bc90043f36
drpatelh <drhpatel@gmail.com>
2020-03-18 13:13:24 +0000

    Add software regexes

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -8,12 +8,25 @@ regexes = {
     'nf-core/covid19': ['v_pipeline.txt', r"(\S+)"],
     'Nextflow': ['v_nextflow.txt', r"(\S+)"],
     'FastQC': ['v_fastqc.txt', r"FastQC v(\S+)"],
-    'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"],
+    'NanoPlot': ['v_nanoplot.txt', r"NanoPlot (\S+)"],
+    'BWA': ['v_bwa.txt', r"Version: (\S+)"],
+    'minimap2': ['v_minimap2.txt', r"(\S+)"],
+    'Samtools': ['v_samtools.txt', r"samtools (\S+)"],
+    'Picard': ['v_picard.txt', r"([\d\.]+)-SNAPSHOT"],
+    'R': ['v_R.txt', r"R version (\S+)"],
+    'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"]
 }
+
 results = OrderedDict()
 results['nf-core/covid19'] = '<span style="color:#999999;\">N/A</span>'
 results['Nextflow'] = '<span style="color:#999999;\">N/A</span>'
 results['FastQC'] = '<span style="color:#999999;\">N/A</span>'
+results['NanoPlot'] = '<span style="color:#999999;\">N/A</span>'
+results['BWA'] = '<span style="color:#999999;\">N/A</span>'
+results['minimap2'] = '<span style="color:#999999;\">N/A</span>'
+results['Samtools'] = '<span style="color:#999999;\">N/A</span>'
+results['Picard'] = '<span style="color:#999999;\">N/A</span>'
+results['R'] = '<span style="color:#999999;\">N/A</span>'
 results['MultiQC'] = '<span style="color:#999999;\">N/A</span>'
 
 # Search each file using its regex
